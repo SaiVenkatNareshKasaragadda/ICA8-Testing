c831e7f767b8c5cb7cec4272ecd56762baa723c4
SaiVenkatNareshKasaragadda <112650297+SaiVenkatNareshKasaragadda@users.noreply.github.com>
2022-10-27 21:24:02 -0700

    Updated readFromFile method

--- ICA8/src/main/java/urinals.java
+++ ICA8/src/main/java/urinals.java
@@ -1,37 +1,57 @@
-/*
-@author Sai Venkat Naresh Kasaragadda, skasara1@asu.edu
- */
 import java.io.*;
 import java.util.ArrayList;
 
+/*
+@author Sai Venkat Naresh Kasaragadda, skasara1@asu.edu
+ */
 public class urinals {
 
-    Boolean goodString(String str) {  // checks to see if valid string
-        char[] arr = str.toCharArray();
-        for (char ch : arr) {
-            if (!Character.isDigit(ch)) {
-                return false;
+        Boolean goodString( String str ) {  // checks to see if valid string
+            char[] arr = str.toCharArray();
+            for(char ch: arr){
+                if(!Character.isDigit(ch)){
+                    return false;
+                }
             }
+            return true;
         }
-        return true;
-    }
-
-    public static int getUrinalsCount(String s) {
-
-        return 0;
-    }
+        public static int getUrinalsCount(String s){
+        
+        }
 
     public static ArrayList<Integer> readFromFile(String path) throws IOException {
-
-        return null;
+        ArrayList<String> inputs = new ArrayList<>();
+        File inputFile = new File(path);
+        FileReader fis = new FileReader(inputFile);
+        BufferedReader br = new BufferedReader(fis);
+        String s;
+        System.out.println(br.readLine());
+        while((s=br.readLine())!=null) {
+            inputs.add(s);
+            System.out.print("Input : "+ s);
+        }
+        ArrayList<Integer> outputs = new ArrayList<>();
+        for(String input : inputs){
+            outputs.add(getUrinalsCount(input));
+            System.out.print("Output : "+ outputs);
+        }
+        return outputs;
     }
 
-    public static void writeToFile(ArrayList<Integer> outputs) throws IOException {
-
+    public static void writeToFile(ArrayList<Integer> outputs) throws IOException{
+        
     }
+
+
 
 
     public static void main(String args[]) throws IOException {
-
+        ArrayList<Integer> output = new ArrayList<>();
+        try{
+            output = readFromFile("input.txt");
+            writeToFile(output);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
