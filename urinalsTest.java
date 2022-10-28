b4fbe5fd48f4d9fc3345e1679fa361584c44ef3a
SaiVenkatNareshKasaragadda <112650297+SaiVenkatNareshKasaragadda@users.noreply.github.com>
2022-10-28 00:01:10 -0700

    Final Urinals.java

--- urinalsTest.java
+++ urinalsTest.java
@@ -10,12 +10,10 @@ class urinalsTest {
     void goodString(){
         urinals urine = new urinals();
         assertEquals(true,urine.goodString("10001"),"====== Sai Venkat Naresh == TEST TWO EXECUTED =======");
-
-
     }
 
     @org.junit.jupiter.api.Test
-    void getUrinalsCountNumbers() {
+    void getUrinalsCountStrings() {
         urinals urine = new urinals();
         assertThrows(NumberFormatException.class,()->urine.getUrinalsCount("abc"));
     }
@@ -30,7 +28,7 @@ class urinalsTest {
     void readFromFileEmptyFile() throws IOException {
         urinals urine = new urinals();
         ArrayList<String> s = new ArrayList<>();
-        assertEquals(s,urine.readFromFile("C:/Users/svnka/ICA8/src/main/java/input.dat"));
+        assertEquals(s,urine.readFromFile("C:/Users/svnka/ICA8/src/main/java/urinal.dat"));
     }
 
     @org.junit.jupiter.api.Test
@@ -44,7 +42,6 @@ class urinalsTest {
         urinals urine = new urinals();
         assertThrows(IOException.class ,()-> urine.readFromFile("C:/Users/svnka/ICA8/main/java/inp.txt"));
     }
-
 
     @org.junit.jupiter.api.Test
     void writeToFile() {
