6c88f1ef3eacad745d457b2f83d78be3461df823
SaiVenkatNareshKasaragadda <112650297+SaiVenkatNareshKasaragadda@users.noreply.github.com>
2022-10-27 20:34:57 -0700

    Urinals.java with skeleton code

--- ICA8/src/main/java/urinals.java
+++ ICA8/src/main/java/urinals.java
@@ -1,10 +1,37 @@
 /*
 @author Sai Venkat Naresh Kasaragadda, skasara1@asu.edu
  */
+import java.io.*;
+import java.util.ArrayList;
+
 public class urinals {
 
-    public static void main(String[] args){
+    Boolean goodString(String str) {  // checks to see if valid string
+        char[] arr = str.toCharArray();
+        for (char ch : arr) {
+            if (!Character.isDigit(ch)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    public static int getUrinalsCount(String s) {
+
+        return 0;
+    }
+
+    public static ArrayList<Integer> readFromFile(String path) throws IOException {
+
+        return null;
+    }
+
+    public static void writeToFile(ArrayList<Integer> outputs) throws IOException {
+
+    }
+
+
+    public static void main(String args[]) throws IOException {
 
     }
 }
