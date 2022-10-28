1075a0b6e744e2a49fe79a317a1f747425c1beb7
SaiVenkatNareshKasaragadda <112650297+SaiVenkatNareshKasaragadda@users.noreply.github.com>
2022-10-27 21:25:00 -0700

    Empty File Test

--- urinalsTest.java
+++ urinalsTest.java
@@ -1,5 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,12 +19,21 @@ class urinalsTest {
     }
 
     @org.junit.jupiter.api.Test
-    void readFromFile() throws IOException {
+    void readFromFileDNotExist() throws IOException {
         urinals urine = new urinals();
-        assertThrows(FileNotFoundException.class, () -> urine.readFromFile("abcde.txt") );
+        ArrayList<String> s = new ArrayList<>();
+        assertThrows(FileNotFoundException.class ,()-> urine.readFromFile("C:/Users/svnka/ICA8/src/main/java/abcde.txt"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void readFromFileEmptyFile() throws IOException {
+        urinals urine = new urinals();
+        ArrayList<String> s = new ArrayList<>();
+        assertEquals(s,urine.readFromFile("C:/Users/svnka/ICA8/src/main/java/input.txt"));
     }
 
     @org.junit.jupiter.api.Test
     void writeToFile() {
+
     }
 }
