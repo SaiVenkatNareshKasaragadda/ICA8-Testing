151fd09b9f872795f5e4e39546b2d62f82d8b7dd
SaiVenkatNareshKasaragadda <112650297+SaiVenkatNareshKasaragadda@users.noreply.github.com>
2022-10-27 21:39:10 -0700

    Updated getUrinalsCount method

--- ICA8/src/main/java/urinals.java
+++ ICA8/src/main/java/urinals.java
@@ -16,8 +16,41 @@ public class urinals {
             return true;
         }
         public static int getUrinalsCount(String s){
-        
+        int[] intArray = new int[s.length()];
+        int res =0;
+        int n = s.length();
+        String[] eachInt = s.split("");
+        for(int i =0;i<n;i++) {
+            intArray[i]= Integer.parseInt(eachInt[i]);
         }
+        for(int i=0;i< n;i++) {
+            if(intArray[i]==0){
+                if(i==0){
+                    if(intArray[i+1]==0 ){
+                    res++;
+                    intArray[i]=1;
+                }}
+                else if(i==n-1){
+                    if(intArray[i-1]==0){
+                    res++;
+                    intArray[i]=1;
+                }}
+                else if(intArray[i-1]==0){
+                    if(intArray[i+1]==0){
+                    res++;
+                    intArray[i]=1;
+                }}
+            }
+        }
+        for(int i=0;i< n-1;i++) {
+            if(intArray[i]==1){
+                if(intArray[i+1]==1){
+                    res = -1;
+                }
+            }
+        }
+        return res;
+    }
 
     public static ArrayList<Integer> readFromFile(String path) throws IOException {
         ArrayList<String> inputs = new ArrayList<>();
@@ -25,7 +58,6 @@ public class urinals {
         FileReader fis = new FileReader(inputFile);
         BufferedReader br = new BufferedReader(fis);
         String s;
-        System.out.println(br.readLine());
         while((s=br.readLine())!=null) {
             inputs.add(s);
             System.out.print("Input : "+ s);
